0a4d619bc5887e27cfc6c259d9b2619c1b530e7d
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-08-21 19:26:32 +0530

    Class 21-08-2020

---
+++ JavaScriptBasics/class2.js
@@ -0,0 +1,14 @@
+//Creating classes Dynamically on-demand
+function makeClass(word) {
+    return class {
+        name = "Luke";//class field
+        sayHi() {//class method linked with .prototype
+            console.log(word);
+        };
+    };
+}
+
+
+let User = makeClass("Hello");
+new User().sayHi();
+console.log(new User().name);
