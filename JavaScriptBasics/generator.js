9b22c2309a43e83012af6db8963341f62b16ae07
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-08-25 10:35:00 +0530

    Update generator.js

--- JavaScriptBasics/generator.js
+++ JavaScriptBasics/generator.js
@@ -6,10 +6,11 @@ function *generator(){
     yield "Maze";
     //return "Hey";
 }
-
 var it = generator();
 var iter = it.next();
 while(iter.done == false){
+    
     console.log(iter);
     iter = it.next();
 }
+console.log(iter);
