3dcb6f3e185bd3dcd59de58e4eb6dc3fb133c6ae
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-10-21 12:07:09 +0530

    Update postSignup.js

--- AuthenticateGAE/handlers/postSignup.js
+++ AuthenticateGAE/handlers/postSignup.js
@@ -1,9 +1,9 @@
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 const { Datastore } = require('@google-cloud/datastore');
 const datastore = new Datastore();//Creates a new client
 async function credentialsInput(req, res) {
     try {
-        const hashPass = await bcrypt.hash(req.body.password, 10);
+        //const hashPass = await bcrypt.hash(req.body.password, 10);
         const kind = "Task"
         const id = "Sample1"
         const taskKey = datastore.key([kind, id]);
@@ -13,11 +13,11 @@ async function credentialsInput(req, res) {
                 time: Date.now().toString(),
                 name: req.body.name,
                 email: req.body.email,
-                password: hashPass
+                password: req.body.password
             }
         };
         await datastore.upsert(obj);
-        console.log(obj);
+        //res.send(obj);
         res.redirect('/login');
     } catch {
         res.redirect('/signup');
