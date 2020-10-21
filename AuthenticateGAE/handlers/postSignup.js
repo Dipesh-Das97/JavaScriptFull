4a434cb7b6398ed4ce45127da8abe945dac35e4e
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-10-21 21:46:35 +0530

    Update postSignup.js

--- AuthenticateGAE/handlers/postSignup.js
+++ AuthenticateGAE/handlers/postSignup.js
@@ -4,11 +4,11 @@ const datastore = new Datastore();//Creates a new client
 async function credentialsInput(req, res) {
     try {
         const hashPass = await bcrypt.hash(req.body.password, 10);
-        const kind = "Task"
-        const id = "Sample1"
-        const taskKey = datastore.key([kind, id]);
+        const kind = "Task";
+        //const id = "Sample2"
+        //const taskKey = datastore.key([kind, id]);
         const obj = {
-            key: taskKey,
+            key: datastore.key([kind, req.body.email]),
             data: {
                 time: Date.now().toString(),
                 name: req.body.name,
