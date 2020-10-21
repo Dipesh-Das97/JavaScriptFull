4cfa966fd0d7a3e8396ce5bb7b7d5ecd214bb808
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-10-21 21:47:05 +0530

    Update postLogin.js

--- AuthenticateGAE/handlers/postLogin.js
+++ AuthenticateGAE/handlers/postLogin.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const datastore = new Datastore();//Creates a new client
 
 async function retrieval(req, res) {
-    const taskKey = datastore.key(['Task', 'Sample1']);
+    const taskKey = datastore.key(['Task', req.body.email]);
     const entity = await datastore.get(taskKey);
     //res.send(entity);
     const result = entity.find(c => (c.email === req.body.email) && bcrypt.compare(req.body.password, c.password));
