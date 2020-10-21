69e7041b525f1bcdfe1cfe4d4152bfce912096b2
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-10-21 13:19:18 +0530

    Update postLogin.js

--- AuthenticateGAE/handlers/postLogin.js
+++ AuthenticateGAE/handlers/postLogin.js
@@ -1,10 +1,12 @@
 const { Datastore } = require('@google-cloud/datastore');
+const bcrypt = require('bcrypt');
 const datastore = new Datastore();//Creates a new client
+
 async function retrieval(req, res) {
     const taskKey = datastore.key(['Task', 'Sample1']);
     const entity = await datastore.get(taskKey);
     //res.send(entity);
-    const result = entity.find(c => (c.email === req.body.email) && (c.password === req.body.password));
+    const result = entity.find(c => (c.email === req.body.email) && bcrypt.compare(req.body.password, c.password));
     if (!result) {
         res.send("Wrong Credential!");
     }
