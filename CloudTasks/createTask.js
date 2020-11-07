33f6593aa6a7be5fd18021952806d6b4cdb8b9f0
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-11-07 10:55:07 +0530

    Update createTask.js

--- CloudTasks/createTask.js
+++ CloudTasks/createTask.js
@@ -1,52 +1,31 @@
-function main(
-    project = 'teleport-intern-dinesh', 
-    queue = 'dinesh-queue', 
-    location = 'us-central1',
-    payload = 'Hello, World!', 
-    inSeconds = 15 // Delay in task execution
-) {
+async function createTask(req, res){
     const { CloudTasksClient } = require('@google-cloud/tasks');
-    const client = new CloudTasksClient({ apiEndpoint: 'http://localhost:8081' });
+    const client = new CloudTasksClient();
+    const project = 'teleport-intern-dinesh';
+    const location = 'us-central1';
+    const queue = 'dinesh-queue';
+    const payload = {
+        name: "Dipesh Das",
+        dept: "EEE"
+    };
+    const inSeconds = 15;
+    const parent = client.queuePath(project, location, queue);
+    const task = {
+        appEngineHttpRequest: {
+            httpMethod: 'POST',
+            relativeUri: '/task',
+            body: payload,
+        },
+        scheduleTime: { inSeconds }
+    };
 
-    async function createTask() {
-        const project = 'teleport-intern-dinesh';
-        const queue = 'dinesh-queue';
-        const location = 'us-central1';
-        const payload = 'We can do this!';
+    const request = {
+        parent: parent,
+        task: task
+    };
 
-        const parent = client.queuePath(project, location, queue);
-        const task = {
-            appEngineHttpRequest : {
-                httpMethod : 'POST',
-                relativeUri : '/task'
-            }
-        };
-
-        if(payload){
-            task.appEngineHttpRequest.body = payload//Buffer.from(payload).toString('base64');
-        }
-
-        if(inSeconds){
-            task.scheduleTime = {second : inSeconds};
-        }
-
-        console.log("Sending Task: ");
-        console.log(task);
-
-        const request = {parent, task};
-        const response = await client.createTask(request);
-        const name = response.name;
-        console.log(`Created task : ${name}`);
-
-
-    }
-    createTask();
-    // [END cloud_tasks_appengine_create_task]
+    const [response] = await client.createTask(request);
+    res.send({ "response": response, "task": task });
 }
 
-/*process.on(unhandledRejection, err => {
-    console.error(err.message);
-    process.exitCode = 1;
-});*/
-
-main();
+module.exports.createTask = createTask;
