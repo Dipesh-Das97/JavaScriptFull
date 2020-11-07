43fa6e4aa8a771bafb3df51732f4c2602cbd1206
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-11-07 10:55:41 +0530

    Update server.js

--- CloudTasks/server.js
+++ CloudTasks/server.js
@@ -2,30 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const func = require('./createTask');
+const createTask = func.createTask;
 
 app.enable('trust proxy');
-app.use(bodyParser.raw({type: 'application/octet-stream'}));
+app.use(bodyParser.raw());
 
 app.get('/', (req, res) => {
     console.log("Hello World!");
     res.send("Hello World!");
 });
 
+
+app.get('/createTask', createTask);
+
+
 app.post('/task', (req, res) => {
-    console.log('Received task with payload: %s', req.body);
-    res.send(`Printed task payload: ${req.body}`).end();
-});
-
-app.get('/task', (req, res) => {
-    res.send(`Printed task payload: ${req.body}`).end();
-    console.log('Received task with payload: %s', req.body);
-});
-
-app.get('*', (req, res) => {
-    res.send('OK').end();
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
     console.log(`listening at port http://localhost:${port}`);
-    console.log(`listening at port http://localhost:${port}/task`);
-})
+    console.log(`listening at port http://localhost:${port}/createTask`);
+});
